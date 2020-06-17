065c9ed9cfe0026bc62809f8f48ffa275ea6d2ab
himanshuj581 <61944409+himanshuj581@users.noreply.github.com>
2020-06-17 14:22:35 +0530

    Update bill_count.py

--- bill_count.py
+++ bill_count.py
@@ -1,5 +1,7 @@
 import math
 def bill_count(totalMoney,billList):
+    if(totalMoney<0):
+        return "Inavalid Total Amount of Money"
     billList.sort()
     restMoney = totalMoney
     billCount = 0
@@ -9,6 +11,8 @@ def bill_count(totalMoney,billList):
             temp = math.floor(restMoney/billList[i])
             billCount += temp
             restMoney -= temp*billList[i]
+         else:
+            return "Invalid Bill Amount"
         i = i-1
     return billCount
     
